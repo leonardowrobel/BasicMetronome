4ad29a072664a9fdfe9db7b21184ccd0ef1280d5
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2024-02-26 22:28:42 -0300

    wip(): MainActivity basic functionality;

--- app/src/main/java/com/basicmetronome/MainActivity.kt
+++ app/src/main/java/com/basicmetronome/MainActivity.kt
@@ -4,23 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.basicmetronome.ui.screens.home.HomeScreen
 import com.basicmetronome.ui.screens.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    //  private fun requestPermissions(): Boolean {}
 
     private val viewModel: HomeViewModel by viewModels()
 
@@ -29,35 +24,11 @@ class MainActivity : ComponentActivity() {
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.bpm
+                viewModel.uiState.value.currentBpm
             }
         }
         setContent {
             HomeScreen(viewModel)
-        }
-    }
-}
-
-@Composable
-fun Greeting(bpm: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "BPM: $bpm",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun Screen(bpm: String, modifier: Modifier = Modifier) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Greeting(bpm)
         }
     }
 }
